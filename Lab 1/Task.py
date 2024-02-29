bfca00508d98663d9a678b05875bd2fbeadc76e4
3amooraz <omarwaleed1907@gmail.com>
2024-02-29 14:26:37 +0200

    added question lastname

--- Lab 1/Task.py
+++ Lab 1/Task.py
@@ -2,13 +2,16 @@ students = []
 num_students = int(input("Enter the number of students: "))
 
 for i in range(num_students):
-    name = input("Enter student name: ")
+    firstname = input("Enter student First name: ")
+    lastnames = input("Enter student Last name: ")
     id = input("Enter student ID: ")
     
-    student_info = {"Name": name, "ID": id}
+    
+    student_info = {"First Name": firstname, "Last Name": lastnames ,"ID": id}
     
     students.append(student_info)
 
 for student in students:
-    print("Name:", student["Name"])
+    print("First Name:", student["First Name"])
+    print("Last Name:", student["Last Name"])
     print("ID:", student["ID"])
